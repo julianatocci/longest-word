bac2614af32e54414334ec9a1cb016065c75c5a4
Juliana Tocci <juliana.tocci@gmail.com>
2025-07-29 00:17:24 +0000

    Feature complete: Dictionary check of attempt

--- src/longest_word/game.py
+++ src/longest_word/game.py
@@ -1,12 +1,14 @@
 
 import string
 import random
+import requests
 
 class Game:
     def __init__(self):
         self.grid = []
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
+
 
     def is_valid(self, word):
         if not word:
@@ -17,4 +19,10 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found']
