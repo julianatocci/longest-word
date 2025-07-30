11a8e8c417d50f7360731c596b0ef7ace2fd6727
Juliana Tocci <juliana.tocci@gmail.com>
2025-07-30 23:41:02 +0000

    Change letter grid background-color to yellow

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
+
 from flask import Flask, render_template, request
 from src.longest_word.game import Game
+# from wsgi import app
+
 
 app = Flask(__name__)
 
@@ -15,3 +18,8 @@ def check():
     word = request.form['word']
     is_valid = game.is_valid(word)
     return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process will serve the app
+    app.run(host='127.0.0.1', port=8080, debug=True)
